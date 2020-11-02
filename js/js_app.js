3b5b7457a559ca66266be71d97c67c8eee5144bf
ANUSORN <anusorn@ANUSORNs-MBP.domain>
2020-11-02 13:56:24 -0700

    Complete Source Code (Website - Nov 02 2020)

---
+++ js/js_app.js
@@ -0,0 +1,26 @@
+var teamMemberObj = {
+    name: ["Don", "Aaron", "Aiyden", "K.T.", "Sam"],
+    position: ["Team Lead", "Writer", "Art", "Marketing", "Programming"]
+};
+
+var personColor = ["#f38181", "#aa96da", "#30e3ca", "#ffcef3", "#393e46"];
+
+
+for(var i = 0; i < teamMemberObj.name.length; i++){
+    console.log("var " + i);
+
+    var numberX = i + 1;
+    var stringNumberX = numberX.toString();
+
+    document.getElementById('person' + stringNumberX).innerHTML = teamMemberObj.name[i];
+    document.getElementById('pos' + stringNumberX).innerHTML = teamMemberObj.position[i];
+
+    document.getElementById("personPic" + stringNumberX).style.color = personColor[i];
+}
+// document.getElementById("person1").innerHTML = teamMemberObj.name[0];
+
+
+
+
+// document.getElementById("personPic1").style.color = personColor[0];
+
